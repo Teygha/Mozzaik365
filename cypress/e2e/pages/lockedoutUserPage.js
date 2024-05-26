c83266ce0f63e3ba42a54878abaa117803865240
mikeekass <michael.ekakitie@testers.testlio.com>
2024-05-26 17:15:38 +0100

    info-to-explain-assertions

--- cypress/e2e/pages/lockedoutUserPage.js
+++ cypress/e2e/pages/lockedoutUserPage.js
@@ -2,13 +2,13 @@ import { sauceSelectors, signIn } from "../../fixtures/pageSelectors"
 export class lockedoutUser {
 
     static loginFlow() {
-        const errorText = 'Epic sadface: Sorry, this user has been locked out.'
+        const errorText = 'Epic sadface: Sorry, this user has been locked out.' //error message when a locked user logs in
         cy.visit("/");
         cy.get(sauceSelectors.loginLogo).contains("Swag Labs");
         cy.get(sauceSelectors.username).click().type(Cypress.env('lockedoutUser'));
         cy.get(sauceSelectors.password).click().type(Cypress.env('password'));
         cy.get(sauceSelectors.loginBtn).click();
         cy.url().should('not.include', '/inventory.html');
-        cy.get(sauceSelectors.errormessage).should('have.text', (errorText));
+        cy.get(sauceSelectors.errormessage).should('have.text', (errorText)); //asserting a locked in user cannot log in and the valid error is displayed
     }
 }
